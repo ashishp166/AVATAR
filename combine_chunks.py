d683d28675c8be8e543ba99843d57faf63960125
tangmonica <mtang662001@gmail.com>
2024-12-15 11:11:07 -0800

    Add avatar inference notebook

--- combine_chunks.py
+++ combine_chunks.py
@@ -1,17 +1,21 @@
 import joblib
 import os
 
-chunks_dir = "avhubert/data/preprocessed_chunks/"
+chunks_dir = "avhubert/data/preprocessed_av_ema"
 
-chunk_files = [os.path.join(chunks_dir, f) for f in sorted(os.listdir(chunks_dir)) if f.startswith("preprocessed_data_chunk")]
+chunk_files = [os.path.join(chunks_dir, f) for f in sorted(os.listdir(chunks_dir))]# if f.startswith("preprocessed_data_chunk")]
 
-combined_data = []
+combined_X_av = []
+combined_X_ema = []
+combined_y = []
 
 for chunk_file in chunk_files:
-    data = joblib.load(chunk_file)
-    combined_data.extend(data)
+    X_av, X_ema, y = joblib.load(chunk_file)
+    combined_X_av.extend(X_av)
+    combined_X_ema.extend(X_ema)
+    combined_y.extend(y)
 
 combined_chunk_file = os.path.join(chunks_dir, "preprocessed_data_combined.pkl")
-joblib.dump(combined_data, combined_chunk_file)
+joblib.dump((combined_X_av, combined_X_ema, combined_y), combined_chunk_file)
 
 print(f"Combined data saved to {combined_chunk_file}")
